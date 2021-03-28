824a68b3f87ac590a5f4360c7b3085ad0934a239
Burhan-Cem <cem.dillioglugil@gmail.com>
2021-03-28 18:46:28 +0200

    Test commit

---
+++ snake_game.py
@@ -0,0 +1,245 @@
+from collections import deque
+from enum import Enum
+import random
+
+
+class Direction(Enum):
+    Y_POSITIVE = 1
+    X_POSITIVE = 2
+    Y_NEGATIVE = 3
+    X_NEGATIVE = 4
+
+
+class OutputInterface:
+    def draw_map(self, game_map):
+        raise NotImplementedError
+
+    def show_game_result(self, game_won):
+        raise NotImplementedError
+
+
+class TextOutput(OutputInterface):
+    def draw_map(self, game_map):
+        dim_x, dim_y = game_map.shape()
+        horizontal_border = '+' + ('-+' * dim_x)
+        print(horizontal_border)
+        for y in range(dim_y):
+            row = '|'
+            for x in range(dim_x):
+                cell = game_map.peek(x, y)
+                cell_type = type(cell)
+                if cell is None:
+                    row += '.'
+                elif cell_type == Snake:
+                    if cell.head_pos() == (x, y):
+                        row += 'O'
+                    else:
+                        row += 'x'
+                elif cell_type == Food:
+                    row += 'F'
+                else:
+                    raise RuntimeError(f'Unknown object class in game_map:{cell_type}')
+                row += '|'
+            print(row)
+        print(horizontal_border)
+
+    def show_game_result(self, game_won):
+        if game_won:
+            print('Game won')
+        else:
+            print('Game lost')
+
+
+class PygameOutput(OutputInterface):
+    def draw_map(self, game_map):
+        raise NotImplementedError
+
+
+class InputInterface:
+    def get_next_action(self):
+        raise NotImplementedError
+
+
+class Keyboard(InputInterface):
+    """Must implement everything in InputInterface"""
+
+    def get_next_action(self):
+        raise NotImplementedError
+
+
+class MLInput(InputInterface):
+    def get_next_action(self):
+        raise NotImplementedError
+
+
+class SnakeGame:
+    def run(self, input_interface, output_interface, dim_x, dim_y, max_iteration=1000000):
+        game_map = Map2D(dim_x, dim_y)
+        snake = Snake(game_map, (dim_x-1) // 2, (dim_y-1) // 2, Direction.X_POSITIVE)
+        max_snake_length = dim_x * dim_y
+
+        Food(game_map)
+
+        output_interface.draw_map(game_map)
+        move_result = snake.move()
+        while move_result not in [Snake.MoveResult.HIT_SNAKE, Snake.MoveResult.HIT_WALL]:
+            if move_result == Snake.MoveResult.ATE_FOOD:
+                if len(snake) == max_snake_length:
+                    output_interface.show_game_result(True)
+                    return
+                Food(game_map)
+
+            output_interface.draw_map(game_map)
+
+            direction = input_interface.get_next_action()
+            snake.set_direction(direction)
+            move_result = snake.move()
+
+            max_iteration -= 1
+            if max_iteration == 0:
+                raise RuntimeError('Max iteration reached.')
+
+        output_interface.show_game_result(False)
+
+
+class Map2D:
+    def __init__(self, dim_x, dim_y):
+        self.map = []
+        for _ in range(dim_x):
+            self.map.append([None]*dim_y)
+        assert dim_x > 0
+        assert dim_y > 0
+
+    def try_put(self, object_to_put, pos_x, pos_y):
+        """Returns None if put is successful. Otherwise returns blocking object."""
+        self.validate_position(pos_x, pos_y)
+        if self.map[pos_x][pos_y] is None:
+            self.map[pos_x][pos_y] = object_to_put
+        else:
+            return self.map[pos_x][pos_y]
+
+    def put(self, object_to_put, pos_x, pos_y):
+        """Returns the object in (pos_x, pos_y) before this function."""
+        self.validate_position(pos_x, pos_y)
+        old_object, self.map[pos_x][pos_y] = self.map[pos_x][pos_y], object_to_put
+        return old_object
+
+    def peek(self, pos_x, pos_y):
+        """Returns object in (pos_x, pos_y)"""
+        self.validate_position(pos_x, pos_y)
+        return self.map[pos_x][pos_y]
+
+    def shape(self):
+        """Returns map dimensions as a tuple"""
+        return len(self.map), len(self.map[0])
+
+    def validate_position(self, pos_x, pos_y):
+        if not self.inside_map_boundary(pos_x, pos_y):
+            raise ValueError(f'Position ({pos_x},{pos_y}) is outside of the current map dimensions')
+
+    def inside_map_boundary(self, pos_x, pos_y):
+        x_max = len(self.map)
+        y_max = len(self.map[0])
+        return 0 <= pos_x < x_max and 0 <= pos_y < y_max
+
+
+class Food:
+    def __init__(self, game_map, max_retries=10000):
+        dim_x, dim_y = game_map.shape()
+
+        def generate_pos(x_max, y_max):
+            x_pos = random.randint(0, x_max - 1)
+            y_pos = random.randint(0, y_max - 1)
+            return x_pos, y_pos
+
+        for i in range(max_retries):
+            if game_map.try_put(self, *generate_pos(dim_x, dim_y)) is None:
+                return
+        raise RuntimeError(f'Cannot place food in game_map in {max_retries} tries')
+
+
+class Snake:
+    def __init__(self, game_map, head_x, head_y, direction):
+        self.direction = direction
+        self.game_map = game_map
+        self.positions = deque()
+        self.positions.append((head_x, head_y))
+
+        blocking_object = game_map.try_put(self, head_x, head_y)
+        if blocking_object:
+            raise ValueError(f'Position ({head_x}, {head_y}) is blocked by {blocking_object}.')
+
+    def __len__(self):
+        return len(self.positions)
+
+    def head_pos(self):
+        return self.positions[0]
+
+    def end_pos(self):
+        return self.positions[-1]
+
+    def set_direction(self, direction):
+        """Changes direction only if it is valid."""
+
+        def same_axis(direction1, direction2):
+            y_axis = [Direction.Y_POSITIVE, Direction.Y_NEGATIVE]
+            x_axis = [Direction.X_POSITIVE, Direction.X_NEGATIVE]
+            return ((direction1 in x_axis and direction2 in x_axis)
+                    or (direction1 in y_axis and direction2 in y_axis))
+
+        if direction is None:
+            return
+        elif not same_axis(self.direction, direction):
+            self.direction = direction
+
+    class MoveResult(Enum):
+        HIT_SNAKE = 1
+        ATE_FOOD = 2
+        HIT_WALL = 3
+        MOVED = 4
+
+    def move(self):
+        """Returns move result."""
+        new_x, new_y = self.positions[0]
+        if self.direction == Direction.Y_POSITIVE:
+            new_y += 1
+        elif self.direction == Direction.X_POSITIVE:
+            new_x += 1
+        elif self.direction == Direction.Y_NEGATIVE:
+            new_y -= 1
+        elif self.direction == Direction.X_NEGATIVE:
+            new_x -= 1
+        else:
+            raise RuntimeError(f'Invalid direction {self.direction}.')
+
+        if not self.game_map.inside_map_boundary(new_x, new_y):
+            return Snake.MoveResult.HIT_WALL
+
+        blocking_object = self.game_map.try_put(self, new_x, new_y)
+        if type(blocking_object) == Snake:
+            if self.end_pos() == (new_x, new_y):
+                self.positions.appendleft((new_x, new_y))
+                self.positions.pop()
+                # TODO: 2 yilan olursa yilanlarin ve mapin uyumlu olmalari lazim
+                return Snake.MoveResult.MOVED
+            else:
+                return Snake.MoveResult.HIT_SNAKE
+        elif type(blocking_object) == Food:
+            self.positions.appendleft((new_x, new_y))
+            self.game_map.put(self, new_x, new_y)
+            return Snake.MoveResult.ATE_FOOD
+        elif blocking_object is None:
+            self.positions.appendleft((new_x, new_y))
+            self.game_map.put(None, *self.positions.pop())
+            return Snake.MoveResult.MOVED
+        raise RuntimeError(f'Blocking object unknown type {type(blocking_object)}')
+
+
+def main():
+    pass
+
+
+
+if __name__ == "__main__":
+    # Executed only if run as a script
+    main()
---
+++ test_snake_game.py
@@ -0,0 +1,403 @@
+import unittest
+import copy
+from unittest.mock import Mock, call, patch
+from snake_game import Food, Snake, Map2D, SnakeGame
+from snake_game import Direction, TextOutput, OutputInterface
+
+
+class FakeOutput(OutputInterface):
+
+    def __init__(self):
+        self.drawn_maps = []
+        self.game_results = []
+
+    def draw_map(self, game_map):
+        self.drawn_maps.append(copy.deepcopy(game_map))
+
+    def show_game_result(self, game_won):
+        self.game_results.append(game_won)
+
+
+class TestTextOutput(unittest.TestCase):
+
+    @patch('builtins.print')
+    def testOutputOfEmptyMap(self, mock_print):
+        game_map = Map2D(2, 2)
+        text_output = TextOutput()
+        text_output.draw_map(game_map)
+        calls = [call('+-+-+'),
+                 call('|.|.|'),
+                 call('|.|.|'),
+                 call('+-+-+')]
+        mock_print.assert_has_calls(calls)
+
+    @patch('builtins.print')
+    def testOutputOfSnakeObjectInMap(self, mock_print):
+        game_map = Map2D(1, 2)
+        snake = Snake(game_map, 0, 0, Direction.Y_POSITIVE)
+        Food(game_map)
+        snake.move()
+        text_output = TextOutput()
+        text_output.draw_map(game_map)
+        calls = [call('+-+'),
+                 call('|x|'),
+                 call('|O|'),
+                 call('+-+')]
+        mock_print.assert_has_calls(calls)
+
+    @patch('builtins.print')
+    def testOutputOfFoodObjectInMap(self, mock_print):
+        game_map = Map2D(1, 1)
+        Food(game_map)
+        text_output = TextOutput()
+        text_output.draw_map(game_map)
+        calls = [call('+-+'),
+                 call('|F|'),
+                 call('+-+')]
+        mock_print.assert_has_calls(calls)
+
+    @patch('builtins.print')
+    def testOutputOfUnknownObjectInMap(self, _mock_print):
+        game_map = Map2D(1, 1)
+        game_map.put('some_object', 0, 0)
+        text_output = TextOutput()
+        with self.assertRaises(RuntimeError) as context:
+            text_output.draw_map(game_map)
+        self.assertIn('Unknown object class', str(context.exception))
+
+    @patch('builtins.print')
+    def testGameResult(self, mock_print):
+        text_output = TextOutput()
+        text_output.show_game_result(True)
+        text_output.show_game_result(False)
+
+        calls = [call('Game won'),
+                 call('Game lost')]
+        mock_print.assert_has_calls(calls)
+
+
+class TestGameLogic(unittest.TestCase):
+
+    @patch('random.randint')
+    def testDrawMap(self, mock_random):
+        mock_input_interface = Mock()
+        mock_input_interface.get_next_action.return_value = Direction.X_POSITIVE
+        fake_output = FakeOutput()
+
+        mock_random.side_effect = [2, 1,
+                                   0, 0]
+        test_game = SnakeGame()
+        test_game.run(mock_input_interface, fake_output, 3, 3)
+        self.assertEqual(2, len(fake_output.drawn_maps))
+
+        def verify_game_map(game_map, object_types_by_pos):
+            max_x, max_y = game_map.shape()
+            for pos_x in range(max_x):
+                for pos_y in range(max_y):
+                    expected_type = object_types_by_pos.get((pos_x, pos_y), type(None))
+                    self.assertEqual(expected_type, type(game_map.peek(pos_x, pos_y)), f'{pos_x} {pos_y}')
+
+        verify_game_map(fake_output.drawn_maps[0], {(1, 1): Snake, (2, 1): Food})
+        verify_game_map(fake_output.drawn_maps[1], {(1, 1): Snake, (2, 1): Snake, (0, 0): Food})
+
+    def testRunHitWallAndDie(self):
+        mock_input_interface = Mock()
+        mock_input_interface.get_next_action.return_value = Direction.X_POSITIVE
+        fake_output = FakeOutput()
+
+        test_game = SnakeGame()
+        test_game.run(mock_input_interface, fake_output, 3, 3)
+        self.assertEqual(fake_output.game_results, [False])
+
+
+"""
+    @patch('builtins.print')
+    @patch('random.randint')
+    def testRunHitSnakeAndDie(self, mock_random, _mock_print):
+        text_output = TextOutput()
+        mock_keyboard = Mock()
+        mock_keyboard.get_next_action.side_effect = [Direction.Y_NEGATIVE,
+                                                     Direction.X_NEGATIVE,
+                                                     Direction.Y_POSITIVE,
+                                                     Direction.Y_POSITIVE,
+                                                     Direction.X_POSITIVE,
+                                                     Direction.Y_NEGATIVE,
+                                                     Direction.X_NEGATIVE
+                                                     ]
+
+        mock_random.side_effect = [2, 1,
+                                   2, 0,
+                                   1, 0,
+                                   1, 1,
+                                   1, 2,
+                                   2, 2,
+                                   0, 0]
+
+        test_game = SnakeGame()
+        game_result = test_game.run(mock_keyboard, text_output, 3, 3)
+        self.assertEqual(game_result, Snake.MoveResult.HIT_SNAKE)
+
+"""
+"""
+    @patch('snake_game.Keyboard.get_next_action')
+    @patch('builtins.print')
+    @patch('random.randint')
+    def testRunOutOfSpace(self, mock_random, _mock_print, mock_keyboard):
+        mock_keyboard.side_effect = [Direction.Y_POSITIVE,
+                                     Direction.X_NEGATIVE,
+                                     Direction.X_NEGATIVE,
+                                     Direction.Y_NEGATIVE,
+                                     Direction.Y_NEGATIVE,
+                                     Direction.X_POSITIVE,
+                                     Direction.X_POSITIVE,
+                                     Direction.X_NEGATIVE,
+                                     Direction.X_POSITIVE,
+                                     Direction.X_POSITIVE
+                                     ]
+        mock_random.side_effect = [2, 1,
+                                   2, 2,
+                                   1, 2,
+                                   0, 2,
+                                   0, 1,
+                                   0, 0,
+                                   1, 0,
+                                   2, 0]
+        test_game = SnakeGame()
+        with self.assertRaises(RuntimeError) as context:
+            test_game.run(Keyboard, TextOutput, 3, 3)
+        self.assertIn('Cannot place food', str(context.exception))
+"""
+
+
+class TestMap2D(unittest.TestCase):
+    def testTriesToPutStringToFreePosition(self):
+        game_map = Map2D(1, 2)
+
+        self.assertIsNone(game_map.try_put('testString', 0, 1))
+        self.assertEqual(game_map.peek(0, 1), 'testString')
+        self.assertIsNone(game_map.peek(0, 0))
+
+    def testTriesToPutStringToOccupiedPosition(self):
+        game_map = Map2D(1, 2)
+        game_map.put('blocker', 0, 1)
+
+        self.assertEqual(game_map.try_put('testString', 0, 1), 'blocker')
+        self.assertEqual(game_map.peek(0, 1), 'blocker')
+        self.assertIsNone(game_map.peek(0, 0))
+
+    def testPutsStringToFreePosition(self):
+        game_map = Map2D(2, 2)
+        game_map.put('testString', 0, 1)
+
+        self.assertEqual(game_map.peek(0, 1), 'testString')
+        self.assertIsNone(game_map.peek(0, 0))
+        self.assertIsNone(game_map.peek(1, 0))
+        self.assertIsNone(game_map.peek(1, 1))
+
+    def testPutsStringToOccupiedPosition(self):
+        game_map = Map2D(2, 2)
+        game_map.put('existing', 0, 1)
+
+        game_map.put('newObject', 0, 1)
+
+        self.assertEqual(game_map.peek(0, 1), 'newObject')
+        self.assertIsNone(game_map.peek(0, 0))
+        self.assertIsNone(game_map.peek(1, 0))
+        self.assertIsNone(game_map.peek(1, 1))
+
+    def testShape(self):
+        game_map = Map2D(1, 5)
+        self.assertEqual(game_map.shape(), (1, 5))
+
+    def testValidatePosition(self):
+        game_map = Map2D(2, 2)
+        game_map.validate_position(0, 1)
+
+    def testInvalidPosition(self):
+        game_map = Map2D(2, 2)
+
+        with self.assertRaises(ValueError) as context:
+            game_map.validate_position(2, 2)
+        self.assertIn('outside of the current map dimensions', str(context.exception))
+
+    def testInsideMapBoundary(self):
+        game_map = Map2D(2, 3)
+        self.assertTrue(game_map.inside_map_boundary(1, 2))
+        self.assertFalse(game_map.inside_map_boundary(2, 0))
+        self.assertFalse(game_map.inside_map_boundary(-4, 0))
+        self.assertFalse(game_map.inside_map_boundary(0, 3))
+        self.assertFalse(game_map.inside_map_boundary(0, -1))
+
+
+class TestFood(unittest.TestCase):
+
+    def testInsertsFoodOnEmptyMap(self):
+        mock_map = Mock()
+        mock_map.shape.return_value = [1, 1]
+        mock_map.try_put.return_value = None
+        food = Food(mock_map)
+        mock_map.try_put.assert_called_once_with(food, 0, 0)
+
+    def testInsertsFoodOnThirdTry(self):
+        mock_map = Mock()
+        mock_map.shape.return_value = [1, 1]
+        mock_map.try_put.side_effect = [object(), object(), None]
+        food = Food(mock_map)
+        calls = [call(food, 0, 0)] * 3
+        mock_map.try_put.assert_has_calls(calls)
+
+    def testRaisesErrorAfterMaxRetries(self):
+        mock_map = Mock()
+        mock_map.shape.return_value = [1, 1]
+        mock_map.try_put.return_value = object()
+        with self.assertRaises(RuntimeError) as context:
+            Food(mock_map, max_retries=3)
+        self.assertIn('Cannot place food', str(context.exception))
+
+    def testInsertsFoodOnValidPosition(self):
+        mock_map = Mock()
+        mock_map.shape.return_value = [2, 2]
+        mock_map.try_put.side_effect = [object()] * 50 + [None]
+        food = Food(mock_map)
+        calls = [call(food, 0, 0), call(food, 1, 0), call(food, 0, 1), call(food, 1, 1)]
+        mock_map.try_put.assert_has_calls(calls, any_order=True)
+
+
+class TestSnake(unittest.TestCase):
+    def testPutsSnakeOnEmptyPosition(self):
+        game_map = Map2D(3, 3)
+        snake = Snake(game_map, 1, 2, Direction.X_POSITIVE)
+        self.assertEqual(game_map.peek(1, 2), snake)
+
+    def testFailsToPutSnakeOnFilledPosition(self):
+        game_map = Map2D(3, 3)
+        game_map.put('blocker', 1, 2)
+        with self.assertRaises(ValueError) as context:
+            Snake(game_map, 1, 2, Direction.X_POSITIVE)
+        self.assertIn('is blocked by', str(context.exception))
+
+    def testSnakeLength(self):
+        game_map = Map2D(2, 1)
+        snake = Snake(game_map, 0, 0, Direction.X_POSITIVE)
+        self.assertEqual(len(snake), 1)
+        Food(game_map)
+        snake.move()
+        self.assertEqual(len(snake), 2)
+
+    def testReturnsHeadPosition(self):
+        game_map = Map2D(3, 3)
+        snake = Snake(game_map, 1, 2, Direction.X_POSITIVE)
+        self.assertEqual(snake.head_pos(), (1, 2))
+
+    def testReturnsEndPosition(self):
+        game_map = Map2D(2, 1)
+        snake = Snake(game_map, 1, 0, Direction.X_NEGATIVE)
+        Food(game_map)
+        snake.move()
+        self.assertEqual(snake.end_pos(), (1, 0))
+
+    def testHeadPositionIsCorrectAfterMove(self):
+        game_map = Map2D(3, 3)
+        snake = Snake(game_map, 1, 1, Direction.X_POSITIVE)
+        snake.move()
+        self.assertEqual(snake.head_pos(), (2, 1))
+
+    def testMovementSuccessfulDirectionChange(self):
+        game_map = Map2D(3, 3)
+        snake = Snake(game_map, 1, 1, Direction.X_POSITIVE)
+        snake.move()
+        snake.set_direction(Direction.Y_POSITIVE)
+        snake.move()
+        self.assertEqual(game_map.peek(2, 2), snake)
+
+    def testMovementUnsuccessfulDirectionChange(self):
+        game_map = Map2D(3, 3)
+        snake = Snake(game_map, 0, 1, Direction.X_POSITIVE)
+        snake.move()
+        snake.set_direction(Direction.X_NEGATIVE)
+        snake.move()
+        self.assertEqual(game_map.peek(2, 1), snake)
+
+    def testMoveXPositive(self):
+        game_map = Map2D(3, 3)
+        snake = Snake(game_map, 1, 1, Direction.X_POSITIVE)
+        self.assertEqual(snake.move(), Snake.MoveResult.MOVED)
+        self.assertIs(game_map.peek(2, 1), snake)
+        self.assertIsNone(game_map.peek(1, 1))
+
+    def testMoveXNegative(self):
+        game_map = Map2D(3, 3)
+        snake = Snake(game_map, 1, 1, Direction.X_NEGATIVE)
+        self.assertEqual(snake.move(), Snake.MoveResult.MOVED)
+        self.assertIs(game_map.peek(0, 1), snake)
+        self.assertIsNone(game_map.peek(1, 1))
+
+    def testMoveYPositive(self):
+        game_map = Map2D(3, 3)
+        snake = Snake(game_map, 1, 1, Direction.Y_POSITIVE)
+        self.assertEqual(snake.move(), Snake.MoveResult.MOVED)
+        self.assertIs(game_map.peek(1, 2), snake)
+        self.assertIsNone(game_map.peek(1, 1))
+
+    def testMoveYNegative(self):
+        game_map = Map2D(3, 3)
+        snake = Snake(game_map, 1, 1, Direction.Y_NEGATIVE)
+        self.assertEqual(snake.move(), Snake.MoveResult.MOVED)
+        self.assertIs(game_map.peek(1, 0), snake)
+        self.assertIsNone(game_map.peek(1, 1))
+
+    def testMoveInUndefinedDirection(self):
+        game_map = Map2D(1, 1)
+        snake = Snake(game_map, 0, 0, 'not_a_direction')
+        with self.assertRaises(RuntimeError) as context:
+            snake.move()
+        self.assertIn('Invalid direction', str(context.exception))
+
+    def testMoveSnakeOnCircularPath(self):
+        game_map = Map2D(2, 2)
+        snake = Snake(game_map, 0, 0, Direction.X_POSITIVE)
+        Food(game_map)
+        Food(game_map)
+        Food(game_map)
+        snake.move()
+        snake.set_direction(Direction.Y_POSITIVE)
+        snake.move()
+        snake.set_direction(Direction.X_NEGATIVE)
+        snake.move()
+        snake.set_direction(Direction.Y_NEGATIVE)
+        self.assertEqual(snake.move(), Snake.MoveResult.MOVED)
+        self.assertEqual(snake.head_pos(), (0, 0))
+        self.assertEqual(snake.end_pos(), (1, 0))
+
+    def testMoveResultHitWall(self):
+        game_map = Map2D(1, 1)
+        snake = Snake(game_map, 0, 0, Direction.Y_NEGATIVE)
+        self.assertEqual(snake.move(), Snake.MoveResult.HIT_WALL)
+
+    def testMoveResultHitSnake(self):
+        game_map = Map2D(2, 1)
+        snake = Snake(game_map, 0, 0, Direction.X_POSITIVE)
+        Snake(game_map, 1, 0, Direction.X_POSITIVE)
+        self.assertEqual(snake.move(), Snake.MoveResult.HIT_SNAKE)
+        self.assertIs(game_map.peek(0, 0), snake)
+
+    def testMoveResultAteFood(self):
+        game_map = Map2D(2, 1)
+        snake = Snake(game_map, 0, 0, Direction.X_POSITIVE)
+        Food(game_map)
+        self.assertEqual(snake.move(), Snake.MoveResult.ATE_FOOD)
+        self.assertIs(game_map.peek(0, 0), snake)
+        self.assertIs(game_map.peek(1, 0), snake)
+        self.assertEqual(snake.head_pos(), (1, 0))
+
+    def testMoveResultHitUnknownObject(self):
+        game_map = Map2D(2, 1)
+        snake = Snake(game_map, 0, 0, Direction.X_POSITIVE)
+        game_map.put('some_object', 1, 0)
+        with self.assertRaises(RuntimeError) as context:
+            snake.move()
+        self.assertIn('Blocking object unknown type', str(context.exception))
+
+
+if __name__ == '__main__':
+    unittest.main()
