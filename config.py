2458fbc98f4b35a86851883f7e00fd89d92aca17
Burhan-Cem <58035164+Burhan-Cem@users.noreply.github.com>
2021-05-18 17:04:42 +0200

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ settings = Dynaconf(
         Validator('dim_x', must_exist=True, gt=0, is_type_of=int),
         Validator('dim_y', must_exist=True, gt=0, is_type_of=int),
 
-        Validator('input_interface', must_exist=True, is_in=['Keyboard', 'MLInput']),
+        Validator('input_interface', must_exist=True, is_in=['Keyboard', 'MLInput', 'RandomInput']),
         Validator('output_interface', must_exist=True, is_in=['Text', 'Pygame']),
 
         Validator('time_step_seconds', when=Validator('input_interface', eq='Keyboard'), must_exist=True, gte=0,
